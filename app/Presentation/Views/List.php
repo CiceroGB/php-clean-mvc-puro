0a9e2a59fc882bbaa943f752dcd64f13ecd89aa5
CiceroGB <brunelli.cg@gmail.com>
2023-06-14 00:12:10 -0300

    feat: ajuste no delete

--- app/Presentation/Views/List.php
+++ app/Presentation/Views/List.php
@@ -9,20 +9,20 @@
 <body>
     <h1>Todo List</h1>
     <a href="http://localhost:8000/teste">Mudar</a>
-        <?php if (empty($todos)) : ?>
-            <p>No todos found.</p>
-        <?php else : ?>
-            <ul>
-                <?php foreach ($todos as $todo) : ?>
-                    <li id="todo-<?= $todo->getId() ?>">
-                        <h2><?= htmlspecialchars($todo->getTitle()) ?></h2>
-                        <!-- O botão de deletar -->
-                        <button type="button" class="btn btn-danger delete-btn'" data-id="<?= $todo->getId() ?>">Delete</button>
-                    </li>
-                <?php endforeach ?>
-            </ul>
-        <?php endif ?>
-        <!-- Aqui você pode colocar um link ou um botão para criar uma nova tarefa -->
+    <?php if (empty($todos)) : ?>
+        <p>No todos found.</p>
+    <?php else : ?>
+        <ul>
+            <?php foreach ($todos as $todo) : ?>
+                <li id="todo-<?= $todo->getId() ?>">
+                    <h2><?= htmlspecialchars($todo->getTitle()) ?></h2>
+                    <!-- O botão de deletar -->
+                    <button type="button" class="btn btn-danger delete-btn" data-id="<?= $todo->getId() ?>">Delete</button>
+                </li>
+            <?php endforeach ?>
+        </ul>
+    <?php endif ?>
+    <!-- Aqui você pode colocar um link ou um botão para criar uma nova tarefa -->
 </body>
 <script>
     $(document).ready(function() {
